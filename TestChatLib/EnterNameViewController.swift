22b61021971fd0628ad5f95542f71745a46217a7
technopancake1 <30537490+technopancake1@users.noreply.github.com>
2018-08-01 09:46:05 -0700

    first

---
+++ TestChatLib/EnterNameViewController.swift
@@ -0,0 +1,34 @@
+//
+//  EnterNameViewController.swift
+//  TestChatLib
+//
+//  Created by Wilson Balderrama on 8/24/17.
+//  Copyright © 2017 Learning. All rights reserved.
+//
+
+import UIKit
+
+class EnterNameViewController: UIViewController {
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "ShowRooms" {
+      let navigationController = segue.destination as? UINavigationController
+      let roomsViewController = navigationController?.viewControllers.first as? ChatRoomsViewController
+      roomsViewController?.currentUser = sender as? User
+    }
+  }
+  
+  @IBAction func enterTouched() {
+    if let username = usernameTextField.text, let password = passwordTextField.text {
+      User.createUser(
+        username: username,
+        password: password
+      ) { [weak self] (error, user) in
+        if error == nil {
+          self?.performSegue(withIdentifier: "ShowRooms", sender: user)
+        }
+      }
+    }
+  }
+}
---
+++ TestChatLib/User.swift
@@ -0,0 +1,67 @@
+//
+//  User.swift
+//  TestChatLib
+//
+//  Created by Wilson Balderrama on 8/24/17.
+//  Copyright © 2017 Learning. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase
+
+struct User {
+  let id: String
+  let username: String
+    let password: String
+}
+
+
+extension User {
+  
+  static let ref = Database.database().reference()
+  
+  static func createUser(
+    username: String, password: String,
+    completion: @escaping (Error?, User?) -> Void
+  ) {
+    let refUsers = ref.child("users")
+    let refUser = refUsers.childByAutoId()
+    
+    var data = [String: Any]()
+    data["username"] = username
+    data["password"] = password
+    data["id"] = refUser.key
+    
+    refUser.setValue(data) { (error, ref) in      
+      if error != nil {
+        completion(error, nil)
+      } else {
+        let user = User(id: refUser.key, username: username, password: password)
+        completion(nil, user)
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
